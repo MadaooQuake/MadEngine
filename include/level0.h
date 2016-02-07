1a75f2e6bf127db9e0b7c4582619eaf0d938de33
Marcin <kielkowskimarcin@prokonto.pl>
2016-02-07 12:54:03 +0100

    move level0 to new class

---
+++ include/level0.h
@@ -0,0 +1,70 @@
+#ifndef LEVEL0_H
+#define LEVEL0_H
+#include <GLFW/glfw3.h>
+
+class level0
+{
+public:
+    level0();
+    virtual ~level0();
+    void generateworld();
+};
+
+level0::level0()
+{
+}
+
+void level0::generateworld()
+{
+    glPushMatrix();
+    glRotatef((float) glfwGetTime() * 100.0f, 0.f, 0.f, 1.f);
+    glBegin(GL_TRIANGLES);
+    glColor3f(0.0f, 0.0f, 1.0f); // Blue
+    glVertex2f(0.1f, -0.6f);
+    glVertex2f(0.7f, -0.6f);
+    glVertex2f(0.4f, -0.1f);
+    glEnd();
+    glPopMatrix();
+
+    glBegin(GL_QUADS);
+    glColor3f(0.37, 0.30, 0.22);
+    glVertex2f(-0.5, 0.6);
+    glVertex2f(0.55, 0.6);
+    glVertex2f(0.55, 0.55);
+    glVertex2f(-0.5, 0.55);
+
+    glVertex2f(-0.55, 0.95);
+    glVertex2f(-0.50, 0.95);
+    glVertex2f(-0.50, -1.0);
+    glVertex2f(-0.55, -1.0);
+
+    glVertex2f(-0.5, 0.3);
+    glVertex2f(0.75, 0.3);
+    glVertex2f(0.75, 0.25);
+    glVertex2f(-0.5, 0.25);
+
+    glVertex2f(-0.2, 0.00);
+    glVertex2f(1.0, 0.00);
+    glVertex2f(1.0, -0.05);
+    glVertex2f(-0.2, -0.05);
+
+    glVertex2f(-0.5, -0.4);
+    glVertex2f(1.0, -0.4);
+    glVertex2f(1.0, -0.45);
+    glVertex2f(-0.5, -0.45);
+
+    glColor3f(0.75, 0.68, 0.59);
+
+    glVertex2f(-0.5, -0.7);
+    glVertex2f(1.0, -0.7);
+    glVertex2f(1.0, -1.0);
+    glVertex2f(-0.5, -1.0);
+    glEnd();
+}
+
+level0::~level0()
+{
+}
+
+
+#endif // LEVEL0_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <iostream>
 #include "include/pinko.h"
+#include "include/level0.h"
 
 static void error_callback(int error, const char* description);
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
@@ -12,6 +13,7 @@ pinko MyPinko;
 
 int main(void)
 {
+    level0 level;
     double position = 0.0f;
     GLFWwindow* window;
     glfwSetErrorCallback(error_callback);
@@ -40,63 +42,7 @@ int main(void)
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
 
-        glPushMatrix();
-        glRotatef((float) glfwGetTime() * 100.0f, 0.f, 0.f, 1.f);
-        glBegin(GL_TRIANGLES);
-        glColor3f(0.0f, 0.0f, 1.0f); // Blue
-        glVertex2f(0.1f, -0.6f);
-        glVertex2f(0.7f, -0.6f);
-        glVertex2f(0.4f, -0.1f);
-        glEnd();
-        glPopMatrix();
-
-        glBegin(GL_QUADS);
-        glColor3f(0.37, 0.30, 0.22);
-        glVertex2f(-0.5, 0.6);
-        glVertex2f(0.55, 0.6);
-        glVertex2f(0.55, 0.55);
-        glVertex2f(-0.5, 0.55);
-
-        glVertex2f(-0.55, 0.95);
-        glVertex2f(-0.50, 0.95);
-        glVertex2f(-0.50, -1.0);
-        glVertex2f(-0.55, -1.0);
-
-        glVertex2f(-0.5, 0.3);
-        glVertex2f(0.75, 0.3);
-        glVertex2f(0.75, 0.25);
-        glVertex2f(-0.5, 0.25);
-
-        glVertex2f(-0.2, 0.00);
-        glVertex2f(1.0, 0.00);
-        glVertex2f(1.0, -0.05);
-        glVertex2f(-0.2, -0.05);
-
-        glVertex2f(-0.5, -0.4);
-        glVertex2f(1.0, -0.4);
-        glVertex2f(1.0, -0.45);
-        glVertex2f(-0.5, -0.45);
-
-        glColor3f(0.75, 0.68, 0.59);
-
-        glVertex2f(-0.5, -0.7);
-        glVertex2f(1.0, -0.7);
-        glVertex2f(1.0, -1.0);
-        glVertex2f(-0.5, -1.0);
-        glEnd();
-
-        // Now draw the triangle
-
-
-//        glPushMatrix();
-//        //glRotatef((float) glfwGetTime() * 100.0f, 10.f, 0.f, 1.f);
-//        glTranslatef((float) glfwGetTime() * 0.5f, 0.f, 0.f);
-//        glBegin(GL_TRIANGLES);
-//        glVertex2f(-0.1f, 0.6f);
-//        glVertex2f(-0.7f, 0.6f);
-//        glVertex2f(-0.4f, 0.1f);
-//        glEnd();
-//        glPopMatrix();
+        level.generateworld();
 
         //another item :D
         position += MyPinko.getMove();
